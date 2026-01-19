5737ec8abc6d9b78c5e7bbdb68d070023d556706
Dillon Hailey <181585418+Velarken@users.noreply.github.com>
2026-01-19 12:50:31 -0500

    Added routes and logic for lowStock and product views

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,8 +1,17 @@
 require('dotenv').config();
 
 function showHome(req,res) {
-    res.render('index', {pageTitle: 'Home Page'})
+    res.render('index', { pageTitle: 'Home Page' })
 }
+function inStockProducts(req,res) {
+    res.render('products', { pageTitle: 'Products In Stock' })
+}
+function showLowStock(req,res) {
+    res.render('lowStock', { pageTitle: 'Low Stock Products' })
+}
+
 module.exports = {
     showHome,
+    inStockProducts,
+    showLowStock
 }
--- routes/appRouter.js
+++ routes/appRouter.js
@@ -3,5 +3,7 @@ const appRouter = Router();
 const appController = require('../controllers/appController.js');
 
 appRouter.get('/', appController.showHome)
+appRouter.get('/products', appController.inStockProducts)
+appRouter.get('/lowStock', appController.showLowStock)
 
 module.exports = appRouter;
